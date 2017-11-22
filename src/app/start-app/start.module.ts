e57918629232e392597aaa42caaa1aef936d3e6d
PoojaGupta <pooja_gupta@msn.com>
2017-11-21 18:39:27 -0600

    added Flex-Layout

--- src/app/start-app/start.module.ts
+++ src/app/start-app/start.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {LayoutModule} from '@angular/cdk/layout';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import {MainMaterialModule} from '../../main-material.module';
 import {StartAppComponent} from './start-app.component';
@@ -19,6 +19,7 @@ import   {START_APP_ROUTES}   from    './routes';
     RouterModule.forChild(START_APP_ROUTES),
     MainMaterialModule,
     LayoutModule,
+    FlexLayoutModule
   ],
   declarations: [
 StartAppComponent,
