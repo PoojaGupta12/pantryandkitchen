24fb2596acfa2dcbc3cb33246e5030e87769fbf1
PoojaGupta <pooja_gupta@msn.com>
2017-11-23 17:30:03 -0600

    bug fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  hide:boolean
+  constructor() {
+    this.hide = true
+  }
 
-  constructor() { }
-
-  hide: true;
   ngOnInit() {
   }
 
